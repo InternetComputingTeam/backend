ff8fcb11164c8f3f3db98649a1650ef0717c452b
Jinyu Zhu <jinyuzhu@smail.nju.edu.cn>
2020-06-15 12:06:42 +0800

    feat: add GroupApi

---
+++ src/main/java/com/example/backend/controller/GroupApi.java
@@ -0,0 +1,55 @@
+package com.example.backend.controller;
+
+import com.example.backend.bl.group.GroupService;
+import com.example.backend.vo.GroupInfoVO;
+import com.example.backend.vo.ResponseVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/group")
+public class GroupApi {
+
+    @Autowired
+    GroupService groupService;
+
+    @PostMapping("/addGroup")
+    public ResponseVO addGroup(@RequestBody GroupInfoVO groupInfoVO) {
+        if (groupService.addGroup(groupInfoVO)) {
+            return ResponseVO.buildSuccess(groupInfoVO);
+        }
+        return ResponseVO.buildFailure("组创建失败");
+    }
+
+    @PostMapping("/delGroup")
+    public ResponseVO delGroup(@RequestBody GroupInfoVO groupInfoVO) {
+        if (groupService.delGroup(groupInfoVO)) {
+            return ResponseVO.buildSuccess(groupInfoVO);
+        }
+        return ResponseVO.buildFailure("组删除失败");
+    }
+
+    @PostMapping("/unbound/{userId}/{groupId}")
+    public ResponseVO unBound(@PathVariable Integer groupId, @PathVariable String userId) {
+        if (groupService.unBoundFromGroup(groupId, userId)) {
+            return ResponseVO.buildSuccess();
+        }
+        return ResponseVO.buildFailure("解绑失败");
+    }
+
+    @PostMapping("/bindToGroup/{userId}/{groupId}")
+    public ResponseVO bindToGroup(@PathVariable Integer groupId, @PathVariable String userId) {
+        if (groupService.bindToGroup(groupId, userId)) {
+            return ResponseVO.buildSuccess();
+        }
+        return ResponseVO.buildFailure("绑定失败");
+    }
+
+    @GetMapping("/queryByUser/{userId}")
+    public ResponseVO queryByUser(@PathVariable String userId) {
+        return ResponseVO.buildSuccess(groupService.queryByUser(userId));
+    }
+
+
+
+}
---
+++ src/main/java/com/example/backend/data/group/GroupMapper.java
@@ -0,0 +1,21 @@
+package com.example.backend.data.group;
+
+import com.example.backend.vo.GroupInfoVO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface GroupMapper {
+
+    Integer addGroup(GroupInfoVO groupInfoVO);
+    Integer delGroup(GroupInfoVO groupInfoVO);
+    Integer unBound(@Param("groupId") Integer groupId);
+    Integer unBoundOfUser(@Param("groupId") Integer groupId, @Param("userId") String userId);
+    Integer bindToGroup(@Param("groupId") Integer groupId, @Param("userId") String userId);
+    List<GroupInfoVO> queryByUser(@Param("userId") String userId);
+
+}
